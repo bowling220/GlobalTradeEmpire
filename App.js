c5896332546008a7a2d9dc6c19ad2b79e110221b
Blaine Oler <olerblaine@gmail.com>
2024-05-13 14:40:57 -0400

    New and Working

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import MainScreen from './client/screens/MainScreen';
-import MarketScreen from './client/screens/MarketScreen';
+import HomeScreen from './HomeScreen';
+import { View } from 'react-native';
+
+
 
 const Stack = createStackNavigator();
+const EmptyHeader = () => {
+  return <View style={{ height: 50 }} />; // Adjust height as needed
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Market" component={MarketScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            header: () => <EmptyHeader />,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+
 
 export default App;
---
+++ colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: 'blue',
+  secondary: 'white',
+  success: 'green',
+  danger: 'red',
+  warning: 'yellow',
+  info: 'teal',
+  light: 'lightgray',
+  dark: 'darkgray',
+};
